d2cbac553d21523ae9569979c58e1007563384a0
Peter Juffernbruch <peterjuffernbruch@gmail.com>
2018-03-18 21:57:01 -0500

    not working

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO sburgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
-INSERT INTO sburgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
-INSERT INTO sburgers (burger_name, devoured) VALUES ('Turkey Burger', FALSE);
+INSERT INTO seq_burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
+INSERT INTO seq_burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO seq_burgers (burger_name, devoured) VALUES ('Turkey Burger', FALSE);
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path")
+var db = require("./models");
+var PORT = process.env.PORT || 3000;
+
+
 
 var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
@@ -23,6 +27,9 @@ app.use("/", routes);
 app.use("/update", routes);
 app.use("/create", routes);
 
-// listen on port 3000
-var port = process.env.PORT || 3000;
-app.listen(port);
+db.sequelize.sync({ force: false }).then(function() {
+  app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+  });
+});
